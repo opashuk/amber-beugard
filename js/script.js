b19f06b50ab060165d271ff5edd4c1413aec6228
opashuk <oleh.pashuk@securekey.com>
2023-01-10 19:44:14 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// Slow presentation of elements.
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  }
+
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.scroll_animation');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+  let options1 = { threshold: [0.5] };
+  let observer1 = new IntersectionObserver(onEntry, options1);
+  let elements1 = document.querySelectorAll('.scroll_animation_right');
+  for (let elm of elements1) {
+    observer1.observe(elm);
+  }
+
+  let options2 = { threshold: [0.5] };
+  let observer2 = new IntersectionObserver(onEntry, options2);
+  let elements2 = document.querySelectorAll('.scroll_animation_left');
+  for (let elm of elements2) {
+    observer2.observe(elm);
+  }
+
+// Slider config
+
+$(document).ready(function(){
+  $('.slider').slick({
+      arrows:false,
+      dots: true,
+      adaptiveHeight: false,
+      slidesToShow: 5,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      pauseOnHover: true,
+      variableWidth: true,
+  });
+});
